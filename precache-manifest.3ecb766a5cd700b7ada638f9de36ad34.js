52bf25093ce6f90b6851402fc6d8f9e5b8968921
Siddhant Jain <siddhantjain15298@gmail.com>
2020-04-19 07:39:26 +0530

    Updates

---
+++ precache-manifest.3ecb766a5cd700b7ada638f9de36ad34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba608bc25ba4f5676d6d47358dc7d661",
+    "url": "/http-request/index.html"
+  },
+  {
+    "revision": "31d655a9800a686adeea",
+    "url": "/http-request/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "19686f463b9e2d403439",
+    "url": "/http-request/static/js/2.0597b762.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/http-request/static/js/2.0597b762.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31d655a9800a686adeea",
+    "url": "/http-request/static/js/main.00677603.chunk.js"
+  },
+  {
+    "revision": "4d1e6fb34e612bc8be8c",
+    "url": "/http-request/static/js/runtime-main.243f3524.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/http-request/static/media/logo.5d5d9eef.svg"
+  }
+]);
